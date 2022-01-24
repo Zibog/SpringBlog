daf08123b0258c007bd0501a54aec0e6ac78cd69
Zibog <dmsidak@gmail.com>
2022-01-24 14:47:18 +0300

    Add displaying of posts

--- src/main/kotlin/com/zibog/blog/controllers/BlogController.kt
+++ src/main/kotlin/com/zibog/blog/controllers/BlogController.kt
@@ -1,13 +1,21 @@
 package com.zibog.blog.controllers
 
+import com.zibog.blog.repo.PostRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class BlogController {
+class BlogController(
+    @Autowired
+    private val postRepository: PostRepository
+) {
+
     @GetMapping("/blog")
     fun blogMain(model: Model): String {
+        val posts = postRepository.findAll()
+        model.addAttribute("posts", posts)
         return "blog-main"
     }
 }
