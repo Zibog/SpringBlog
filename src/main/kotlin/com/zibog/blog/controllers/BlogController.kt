6f5c88abdf01ecb30263fd5e643a775869b03ec9
Zibog <dmsidak@gmail.com>
2022-01-24 16:13:25 +0300

    Add detailed post view

--- src/main/kotlin/com/zibog/blog/controllers/BlogController.kt
+++ src/main/kotlin/com/zibog/blog/controllers/BlogController.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 
@@ -36,5 +37,14 @@ class BlogController(
         val post = Post(title, anons, fullText)
         postRepository.save(post)
         return "redirect:/blog"
+    }
+
+    @GetMapping("/blog/{id}")
+    fun blogDetails(@PathVariable("id") id: Long, model: Model): String {
+        val post = postRepository.findById(id)
+        val res = ArrayList<Post>()
+        post.ifPresent(res::add)
+        model.addAttribute("post", res)
+        return "blog-details"
     }
 }
